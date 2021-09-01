f49d02886bbaad26168c7dfa2c415e046845cb5d
RafaCypherpunk <rafafuentesrangel@outlook.com>
2021-09-01 14:27:44 -0500

    Commit v1.0

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin")
+
+module.export = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        filename: "index.js",
+        path: path.resolve(__dirname,  "dist")
+    },
+
+    plugins: [
+        new CopyPlugin([
+            {
+                from: ".src/index.html",
+                to: "index.html"
+            }
+        ])
+    ],
+
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true
+    }
+}
